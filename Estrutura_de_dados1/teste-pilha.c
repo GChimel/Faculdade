f570c91c7608b4ad52c28e4a2be2d3560587afd6
GChimel <gustdeveloper@gmail.com>
2023-06-02 13:00:14 -0300

    código com erro

---
+++ Estrutura_de_dados1/teste-pilha.c
@@ -0,0 +1,26 @@
+//Código com errro
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NO {
+    int dado;
+    NO* prox;
+}NO;
+
+typedef struct PILHA {
+    NO* topo;
+}PILHA;
+
+PILHA *p;
+
+int foo(){
+    NO* ptr = p->topo;
+    int dado;
+    if (ptr != NULL) {
+        p -> topo = ptr -> prox;
+        ptr -> prox = NULL;
+        dado = ptr -> dado;
+        free(ptr);
+        return dado;
+    }
+}
